66ebe1c7a51f57d903b756f7c7a71583f80924c7
Md Robin Rana <robin4500bd@gmail.com>
2021-10-27 23:08:08 +0600

    d

--- index.js
+++ index.js
@@ -28,12 +28,6 @@ async function insertData()
             res.send('This is the server root');
         });
 
-        // Test api
-        app.get('/test', async (req, res) =>
-        {
-            res.send('Hello test api');
-        });
-
         // POST API
         app.post('/api/products', async (req, res) =>
         {
